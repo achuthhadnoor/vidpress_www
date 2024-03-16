e42e1e6978450aaba78f32360782b7193e78dca4
achuthhadnoor <armorachyuth123@gmail.com>
2024-03-16 13:32:33 +0530

    license screen

---
+++ hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useEffect, useState } from "react"
+
+const useLocalStorage = () => {
+    const [user, setUser] = useState<string | null>(null);
+    const updateUser = (newUser: any) => {
+        localStorage.setItem('user', JSON.stringify(user))
+        setUser(newUser);
+    }
+    useEffect(() => {
+        if (window) {
+            const userData = window.localStorage.getItem('user') || null;
+            setUser(userData);
+        }
+    }, [])
+
+    return {
+        user,
+        updateUser
+    }
+}
+
+export default useLocalStorage;
